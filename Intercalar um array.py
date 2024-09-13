1a9afc24fc411b5a5306342b556cdf13eb64e275
Fariofa <164511695+M1zeria@users.noreply.github.com>
2024-09-13 10:58:04 -0300

    Create Intercalar um array.py

---
+++ Intercalar um array.py
@@ -0,0 +1,16 @@
+n = int(input())
+lista1 = []
+lista2 = []
+for i in range (n):
+    num = int(input())
+    lista1.append(num)
+    
+for i in range (n):
+    num = int(input())
+    lista2.append(num)
+
+for i in range(n):
+    print(lista1[i])
+    print(lista2[i])
+    
+
